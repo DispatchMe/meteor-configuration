666dbdcf84f575e57bda87188228f66ac0b831a2
Jason Raede <jason.raede@gmail.com>
2015-09-22 13:31:17 -0400

    Minor fixes for prefix support

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:configuration',
   summary: 'App configuration manager with inheritance',
-  version: '0.0.2'
+  version: '0.0.3'
 });
 
 Package.onUse(function(api) {
