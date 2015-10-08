117af0d51c5e3a38f946a95eedd8b23a3aad418b
Eric Dobbertin <eric@dairystatedesigns.com>
2015-10-08 07:57:10 -0500

    Bump version and publish

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:configuration',
   summary: 'App configuration manager with inheritance',
-  version: '0.1.0'
+  version: '0.1.1'
 });
 
 Package.onUse(function(api) {
