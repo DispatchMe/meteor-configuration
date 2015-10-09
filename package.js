c14f37f431e4ec1378376c0d5de3f373ac42db03
Eric Dobbertin <eric@dairystatedesigns.com>
2015-10-08 22:06:28 -0500

    Fix error and publish

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:configuration',
   summary: 'App configuration manager with inheritance',
-  version: '0.1.3'
+  version: '0.1.4'
 });
 
 Package.onUse(function(api) {
