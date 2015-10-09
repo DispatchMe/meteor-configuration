d5fd99fcb1fe894cec9d9936220b29b2b4b57e34
Eric Dobbertin <eric@dairystatedesigns.com>
2015-10-09 10:20:04 -0500

    Fix setForEntity and publish

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:configuration',
   summary: 'App configuration manager with inheritance',
-  version: '0.1.4'
+  version: '0.1.5'
 });
 
 Package.onUse(function(api) {
